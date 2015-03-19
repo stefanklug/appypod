790f3d44955f52ad3497dba2ec9415e1bb4fa6c9
Gaetan Delannay <gaetan.delannay@gmail.com>
2015-03-19 11:01:26 +0100

    [gen] Bugfix: ckeditor with ie8.

--- gen/ui/ckeditor/styles.js
+++ gen/ui/ckeditor/styles.js
@@ -2,7 +2,4 @@
  * Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
-CKEDITOR.stylesSet.add('default',[
- { name: 'Styled image (left)', element:'img', attributes: {'class': 'left'}},
- { name: 'Styled image (right)', element:'img', attributes: {'class': 'right'}},
-]);
+CKEDITOR.stylesSet.add('default',[]);
