92e975f74277bb7edb4fc4201352ea184bffcbb5
Stefan Klug <klug.stefan@gmx.de>
2015-11-03 21:02:10 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,60 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='appypod',
+    version='0.9.3',
+    description='AppyPod',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/stefanklug/appy',
+
+    # Author details
+    author='Gaëtan Delannay, modified by Stefan Klug',
+    author_email='klug.stefan@gmx.de',
+
+    # Choose your license
+    license='LGPL-2',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='odf template odf development',
+    packages=find_packages(exclude=[])
+)
